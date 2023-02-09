da7757de82bcb60a0fe59e0797ae4042da17f3e4
Isaque-Morais <isaquemorais5@gmail.com>
2023-02-09 12:02:12 -0300

    Origanizando testes com Markers

--- python_tdd/tests/test_bytebank.py
+++ python_tdd/tests/test_bytebank.py
@@ -1,5 +1,6 @@
 from codigo.bytebank import Funcionario
 import pytest
+from pytest import mark
 
 class TestClass:
     def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
@@ -41,6 +42,7 @@ class TestClass:
         #Then
         assert resultado == esperado 
 
+    # @mark.calcular_bonus
     def test_quando_calcular_bonus_receber_10000_deve_retorna_100(self):
 
         #Given
@@ -54,7 +56,8 @@ class TestClass:
         #Then
         assert resultado == esperado
 
-    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_exepection(self):
+    # @mark.calcular_bonus
+    def test_quando_calcular_bonus_recebe_1000000_deve_retornar_expection(self):
         #Given
         with pytest.raises(Exception):
             entrada = 1000000
